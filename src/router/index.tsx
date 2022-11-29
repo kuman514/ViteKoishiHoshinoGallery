ec193d2de23026ab315b6fa02a4ffd62e7ad4c43
kumankoishi.py <7iggif@naver.com>
2022-11-30 05:57:35 +0900

    refactor(route): reduce hardcode route elements

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
-import Home from '^/components/pages/Home';
 import Main from '^/Main';
+import Home from '^/components/pages/Home';
 import Koishi from '^/components/pages/Koishi';
 import Hoshino from '^/components/pages/Hoshino';
 import ErrorPage from '^/components/pages/ErrorPage';
 
+import { RouteName, RouteMatch } from '^/types';
+
+const element: Record<RouteName, ReactNode> = {
+  [RouteName.HOME]: <Home />,
+  [RouteName.KOISHI]: <Koishi />,
+  [RouteName.HOSHINO]: <Hoshino />,
+};
+
+const routeElements = (Object.keys(RouteMatch) as Array<RouteName>).map((key) => (
+  <Route key={key} path={RouteMatch[key]} element={element[key]} />
+));
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Main />} errorElement={<ErrorPage />}>
-      <Route path="/" element={<Home />} />
-      <Route path="koishi" element={<Koishi />} />
-      <Route path="hoshino" element={<Hoshino />} />
+      { routeElements }
     </Route>,
   ),
 );
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,11 @@
-export enum RouteNames {
-  HOME = '/',
-  KOISHI = '/koishi',
-  HOSHINO = '/hoshino',
+export enum RouteName {
+  HOME = 'HOME',
+  KOISHI = 'KOISHI',
+  HOSHINO = 'HOSHINO',
 }
+
+export const RouteMatch: Record<RouteName, string> = {
+  [RouteName.HOME]: '/',
+  [RouteName.KOISHI]: 'koishi',
+  [RouteName.HOSHINO]: 'hoshino',
+};
