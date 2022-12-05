74604bc0954db3f17811cdb518b72e6927ecaa84
kumankoishi.py <7iggif@naver.com>
2022-12-05 20:28:27 +0900

    refactor(home-button): reduce magic strings

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,18 +1,20 @@
 import React, { FC } from 'react';
 import { useNavigate } from 'react-router-dom';
 import HomeSelection from '^/components/atoms/HomeSelection';
+import color from '^/constants/color';
 
 import KoishiImg from '^/assets/home/koishi.png';
 import HoshinoImg from '^/assets/home/hoshino.png';
+import { RouteMatch, RouteName, RouteTitle } from '^/types';
 
 const Home: FC<{}> = () => {
   const navigate = useNavigate();
   return (
     <div>
       <HomeSelection
-        title="Koishi"
-        btnColor="#20CC20"
-        onClick={() => navigate('koishi')}
+        title={RouteTitle[RouteName.KOISHI]}
+        btnColor={color.koishiTheme}
+        onClick={() => navigate(RouteMatch[RouteName.KOISHI])}
       >
         <img
           src={KoishiImg}
@@ -20,9 +22,9 @@ const Home: FC<{}> = () => {
         />
       </HomeSelection>
       <HomeSelection
-        title="Hoshino"
-        btnColor="#DD90BB"
-        onClick={() => navigate('hoshino')}
+        title={RouteTitle[RouteName.HOSHINO]}
+        btnColor={color.hoshinoTheme}
+        onClick={() => navigate(RouteMatch[RouteName.HOSHINO])}
       >
         <img
           src={HoshinoImg}
---
+++ src/constants/color.ts
@@ -0,0 +1,5 @@
+export default {
+  koishiTheme: '#20CC20',
+  hoshinoTheme: '#DD90BB',
+  darkBackground: '#282C34',
+};
