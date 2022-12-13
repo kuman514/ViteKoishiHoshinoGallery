b4095c83ec2bc0f09d36c659d4e2a5a6d1ae8a6f
kumankoishi.py <7iggif@naver.com>
2022-12-13 20:58:20 +0900

    feat(fetch): configure axios get configs

--- src/types/index.ts
+++ src/types/index.ts
@@ -15,3 +15,24 @@ export const RouteTitle: Record<RouteName, string> = {
   [RouteName.KOISHI]: 'Koishi',
   [RouteName.HOSHINO]: 'Hoshino',
 };
+
+export enum ContentType {
+  H1 = 'h1',
+  H2 = 'h2',
+  PARAGRAPH = 'paragraph',
+  IMAGE = 'image',
+  YOUTUBE = 'youtube',
+}
+
+export interface Content {
+  type: ContentType;
+  content: string;
+  alt?: string;
+}
+
+export interface GetContentResponse {
+  type: RouteName;
+  firstUpdate: string;
+  lastUpdate: string;
+  contents: Content[];
+}
