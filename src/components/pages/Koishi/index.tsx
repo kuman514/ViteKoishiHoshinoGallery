08816ec6fca7a072ee7300a9ead8e17e0ef6564f
kumankoishi.py <7iggif@naver.com>
2022-12-13 20:59:35 +0900

    feat(fetch): test axios get api

--- src/components/pages/Koishi/index.tsx
+++ src/components/pages/Koishi/index.tsx
@@ -1,7 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import axios from 'axios';
+import { GetContentResponse } from '^/types';
+import { VITE_API_URL } from '^/constants/url';
 
-const Koishi: FC<{}> = () => (
-  <>Koishi</>
-);
+const Koishi: FC<{}> = () => {
+  useEffect(() => {
+    // temporarily using axios here
+    (async () => {
+      const { data } = await axios.get<GetContentResponse>(`${VITE_API_URL}/koishi`);
+      console.log(data);
+    })();
+  }, []);
+
+  return (
+    <>Koishi</>
+  );
+};
 
 export default Koishi;
