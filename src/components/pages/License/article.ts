522d1ec63de282785a537852ca4888867ab48336
kumankoishi.py <7iggif@naver.com>
2022-12-20 05:09:04 +0900

    docs(license): finish adding license component

---
+++ src/components/pages/License/article.ts
@@ -0,0 +1,37 @@
+export const licenseArticle = {
+  react: {
+    title: 'React',
+    copyright: 'Copyright (c) Meta Platforms, Inc. and affiliates.',
+    license: 'MIT License',
+  },
+  vite: {
+    title: 'Vite',
+    copyright: 'Copyright (c) 2019-present, Yuxi (Evan) You and Vite contributors',
+    license: 'MIT License',
+  },
+  typescript: {
+    title: 'TypeScript',
+    copyright: 'Copyright (c) Microsoft',
+    license: 'Apache License',
+  },
+  axios: {
+    title: 'Axios',
+    copyright: 'Copyright (c) 2014-present Matt Zabriskie & Collaborators',
+    license: 'MIT License',
+  },
+  eslint: {
+    title: 'ESLint',
+    copyright: 'Copyright OpenJS Foundation and other contributors',
+    license: 'MIT License',
+  },
+  'styled-components': {
+    title: 'styled-components',
+    copyright: 'Copyright (c) 2016-present Glen Maddern and Maximilian Stoiber',
+    license: 'MIT License',
+  },
+  'react-router-dom': {
+    title: 'react-router-dom',
+    copyright: 'Copyright (c) 2021 Ernesto Garcia',
+    license: 'MIT License',
+  },
+};
--- src/components/pages/License/index.tsx
+++ src/components/pages/License/index.tsx
@@ -1,40 +1,27 @@
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 import styled from 'styled-components';
+import { licenseArticle } from './article';
 
 const Title = styled.h2`
   margin-top: 20px;
 `;
 
-const License: FC<{}> = () => (
-  <div>
-    <Title>React</Title>
-    <div>Copyright (c) Meta Platforms, Inc. and affiliates.</div>
-    <div>MIT License</div>
+const License: FC<{}> = () => {
+  const renderItems: ReactNode = Object.entries(licenseArticle).map(
+    ([key, { title, copyright, license }]) => (
+      <div key={key}>
+        <Title>{ title }</Title>
+        <div>{ copyright }</div>
+        <div>{ license }</div>
+      </div>
+    ),
+  );
 
-    <Title>Vite</Title>
-    <div>Copyright (c) 2019-present, Yuxi (Evan) You and Vite contributors</div>
-    <div>MIT License</div>
-
-    <Title>TypeScript</Title>
-    <div>Copyright (c) Microsoft</div>
-    <div>Apache License</div>
-
-    <Title>Axios</Title>
-    <div>Copyright (c) 2014-present Matt Zabriskie & Collaborators</div>
-    <div>MIT License</div>
-
-    <Title>ESLint</Title>
-    <div>Copyright OpenJS Foundation and other contributors</div>
-    <div>MIT License</div>
-
-    <Title>styled-components</Title>
-    <div>Copyright (c) 2016-present Glen Maddern and Maximilian Stoiber</div>
-    <div>MIT License</div>
-
-    <Title>react-router-dom</Title>
-    <div>Copyright (c) 2021 Ernesto Garcia</div>
-    <div>MIT License</div>
-  </div>
-);
+  return (
+    <div>
+      { renderItems }
+    </div>
+  );
+};
 
 export default License;
